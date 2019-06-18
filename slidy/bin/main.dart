c9dfe57cc7af07a56c342aba46b7deda971d952a
jacobmoura7 <tecleigo@gmail.com>
2019-06-18 04:00:04 -0300

    push

--- slidy/bin/main.dart
+++ slidy/bin/main.dart
@@ -2,7 +2,7 @@ import 'package:slidy/slidy.dart';
 
 main(List<String> args) {
   if(args[0] == "generate" || args[0] == "g"){
-    Generate(args[1]).module();
+    Generate(args);
   }
 
 }
--- slidy/lib/src/generate.dart
+++ slidy/lib/src/generate.dart
@@ -1,10 +1,19 @@
 class Generate {
 
-  final String path;
 
-  Generate(this.path);
+  Generate(args){
+    if(args[1] == 'module' || args[1] == 'm'){
+       module(args[2]);
+    } else if(args[1] == 'component' || args[1] == 'c'){
+       component(args[2]);
+    } 
+  }
 
-  module(){
+  module(String path){
+    print("$path");
+  }
+
+  component(String path){
     print("$path");
   }
   
