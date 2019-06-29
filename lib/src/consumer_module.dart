e51e92c6e59c4bae57d3a717a923463f20ea49ce
Jacob Moura <tecleigo@gmail.com>
2019-06-29 13:51:49 -0300

    add doc to ModuleWidget

--- lib/src/consumer_module.dart
+++ lib/src/consumer_module.dart
@@ -2,8 +2,5 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 
 class ConsumerModule<T extends BlocBase , R extends ModuleWidget> extends Consumer<T> {
-  
-  final Widget Function(BuildContext context, T value) builder;
-
-  ConsumerModule({Key key, @required this.builder}) : super(key: key, builder: builder, tag: R.toString());
+  ConsumerModule({Key key, @required Widget Function(BuildContext context, T value) builder}) : super(key: key, builder: builder, tag: R.toString());
 }
