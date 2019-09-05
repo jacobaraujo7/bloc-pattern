369745bbbb5b6da0351ddca6c0a74587157928e3
Pedro Bissonho <pedro.bissonho@gmail.com>
2019-09-04 21:28:35 -0300

    add disposable implements on BlocBase

--- lib/src/bloc_base.dart
+++ lib/src/bloc_base.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/foundation.dart';
 
-abstract class BlocBase extends ChangeNotifier {}
+abstract class BlocBase extends ChangeNotifier implements Disposable {}
