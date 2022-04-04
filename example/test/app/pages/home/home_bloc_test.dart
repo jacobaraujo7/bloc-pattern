7e4d02c55d03aa6eb299a6439b9301a57d2988b7
Vilson Blanco Dauinheimer <Bwolfnoob@gmail.com>
2022-04-04 00:00:43 -0300

    Fix Enviroment version

--- example/test/app/pages/home/home_bloc_test.dart
+++ example/test/app/pages/home/home_bloc_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:exampleinjector/app/pages/home/home_bloc.dart';
 
 void main() {
-  HomeBloc bloc;
+ late HomeBloc bloc;
 
   setUp(() {
     bloc = HomeBloc();
